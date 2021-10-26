b9628373efd76f5fff3de80a14aad65b38cd3a82
Josue Lugaro <josuelugaro15@gmail.com>
2021-10-26 18:30:33 -0500

    Added comments to state

---
+++ app/api/comment_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, jsonify, session, request
+from app.models import Comment, db
+from app.forms import CommentForm
+from flask_login import current_user, login_required
+
+comment_routes = Blueprint('comment', __name__)
+
+@comment_routes.route('/')
+@login_required
+def get_all_comments():
+    comments = Comment.query.all()
+    test = [comment.to_dict() for comment in comments]
+    print(test, "<-----------------------------------------")
+    return {"comments": [comment.to_dict() for comment in comments]}
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,3 +1,4 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .duck_form import DuckForm
+from .comment_form import CommentForm
