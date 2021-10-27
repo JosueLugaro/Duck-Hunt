a18a449e744422d148fd4a08bce10ec850ca0ec7
Josue Lugaro <josuelugaro15@gmail.com>
2021-10-27 18:46:13 -0500

    Positioned the comment options dropdown correctly

--- react-app/src/components/Comment.js
+++ react-app/src/components/Comment.js
@@ -15,44 +15,46 @@ export default function Comment({ commentId }) {
             onMouseOver={() => currentUser.id === comment.user_id ? setMouseOver('mouse-over') : null}
             onMouseLeave={() => currentUser.id === comment.user_id ? setMouseOver('') : null}
         >
-            <div className="user-info">
+            <div className="comment-user-profile-pic-container">
                 <img src={comment.user.profile_pic} alt="user-profile-pic"className="comment-user-profile-pic"/>
             </div>
-            <div>
-                <div className="comment-username">{comment.user.username}</div>
+            <div className="everything-but-the-user-profile-pic">
+                <div className="username-and-options-container">
+                    <div className="comment-username">{comment.user.username}</div>
+                    <div className="options-container" onMouseLeave={() => setIsOpen('')}>
+                        <div
+                            className="options-icon-container"
+                            onClick={(e) => {
+                                e.stopPropagation()
+                                return isOpen ? setIsOpen('') : setIsOpen('open')
+                            }}
+                        >
+                            {currentUser.id === comment.user_id ? <span className={`material-icons post-options-icon ${mouseOver}`}>more_horiz</span> : null}
+                        </div>
+                        <div className={`dropdown-options ${isOpen}`}>
+                            <div
+                                className="post-option"
+                                onClick={(e) => {
+                                    e.stopPropagation()
+                                    // return openUpdateFormModal(post.id)
+                                }}
+                            >
+                                <p>Edit</p>
+                            </div>
+                            <div
+                                className="post-option"
+                                onClick={(e) => {
+                                    e.stopPropagation()
+                                    // return openDeletionConfirmationModal(post.id)
+                                }}
+                            >
+                                <p>Delete</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="comment-content">
                     {comment.content}
-                </div>
-            </div>
-            <div className="options-container" onMouseLeave={() => setIsOpen('')}>
-                <div
-                    className="options-icon-container"
-                    onClick={(e) => {
-                        e.stopPropagation()
-                        return isOpen ? setIsOpen('') : setIsOpen('open')
-                    }}
-                >
-                    {currentUser.id === comment.user_id ? <span className={`material-icons post-options-icon ${mouseOver}`}>more_horiz</span> : null}
-                </div>
-                <div className={`dropdown-options ${isOpen}`}>
-                    <div
-                        className="post-option"
-                        onClick={(e) => {
-                            e.stopPropagation()
-                            // return openUpdateFormModal(post.id)
-                        }}
-                    >
-                        <p>Edit</p>
-                    </div>
-                    <div
-                        className="post-option"
-                        onClick={(e) => {
-                            e.stopPropagation()
-                            // return openDeletionConfirmationModal(post.id)
-                        }}
-                    >
-                        <p>Delete</p>
-                    </div>
                 </div>
             </div>
         </div>
