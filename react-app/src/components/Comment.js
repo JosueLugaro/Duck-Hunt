d320a684acc29d009941e8b0bdd46eef86018721
Josue Lugaro <josuelugaro15@gmail.com>
2021-10-27 18:36:09 -0500

    Added options dropdown to comments

--- react-app/src/components/Comment.js
+++ react-app/src/components/Comment.js
@@ -1,19 +1,58 @@
-import React from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import './Comment.css';
 
 export default function Comment({ commentId }) {
+    let currentUser = useSelector(state => state.session.user);
     let comments = useSelector(state => state.Comments);
+    const [isOpen, setIsOpen] = useState('');
+    const [mouseOver, setMouseOver] = useState('');
+    let comment = comments[commentId]
 
     return (
-        <div className="comment">
+        <div
+            className="comment"
+            onMouseOver={() => currentUser.id === comment.user_id ? setMouseOver('mouse-over') : null}
+            onMouseLeave={() => currentUser.id === comment.user_id ? setMouseOver('') : null}
+        >
             <div className="user-info">
-                <img src={comments[commentId].user.profile_pic} alt="user-profile-pic"className="comment-user-profile-pic"/>
+                <img src={comment.user.profile_pic} alt="user-profile-pic"className="comment-user-profile-pic"/>
             </div>
             <div>
-                <div className="comment-username">{comments[commentId].user.username}</div>
+                <div className="comment-username">{comment.user.username}</div>
                 <div className="comment-content">
-                    {comments[commentId].content}
+                    {comment.content}
+                </div>
+            </div>
+            <div className="options-container" onMouseLeave={() => setIsOpen('')}>
+                <div
+                    className="options-icon-container"
+                    onClick={(e) => {
+                        e.stopPropagation()
+                        return isOpen ? setIsOpen('') : setIsOpen('open')
+                    }}
+                >
+                    {currentUser.id === comment.user_id ? <span className={`material-icons post-options-icon ${mouseOver}`}>more_horiz</span> : null}
+                </div>
+                <div className={`dropdown-options ${isOpen}`}>
+                    <div
+                        className="post-option"
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            // return openUpdateFormModal(post.id)
+                        }}
+                    >
+                        <p>Edit</p>
+                    </div>
+                    <div
+                        className="post-option"
+                        onClick={(e) => {
+                            e.stopPropagation()
+                            // return openDeletionConfirmationModal(post.id)
+                        }}
+                    >
+                        <p>Delete</p>
+                    </div>
                 </div>
             </div>
         </div>
